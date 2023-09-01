d17b6e3166f74a723c58a35d1fb9bc946ef16a2b
snunes92 <82555961+snunes92@users.noreply.github.com>
2023-09-01 17:59:48 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,7 +1,14 @@
-
+document.addEventListener("DOMContentLoaded", function () {//nos aseguramos de que la pagina cargue antes de correr nuestro codigo
     const miDiv = document.getElementById("miDiv");
+    const saludarButton = document.getElementById("saludarButton");
 
-    miDiv.addEventListener("click", function () {
+    miDiv.addEventListener("click", function (event) {
+        event.stopPropagation(); // Detiene la propagación del evento en el div
         alert("Hola! Soy el div");
     });
 
+    saludarButton.addEventListener("click", function (event) {
+        event.stopPropagation(); // Detiene la propagación del evento en el botón
+        alert("Hola!");
+    });
+});
