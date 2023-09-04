3a167ad6669c375c6fe6d00a1dcff0a070154727
snunes92 <82555961+snunes92@users.noreply.github.com>
2023-09-03 23:45:29 -0300

    DOMContentLoaded

--- eventos.js
+++ eventos.js
@@ -1,7 +1,7 @@
-
+document.addEventListener("DOMContentLoaded", function () {//nos aseguramos de que la pagina cargue antes de correr nuestro codigo
     const miDiv = document.getElementById("miDiv");
 
     miDiv.addEventListener("click", function () {
         alert("Hola! Soy el div");
     });
-
+};
